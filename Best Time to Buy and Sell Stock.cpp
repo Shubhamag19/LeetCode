a9651fb175b21c4e74707887b2cb7832454441e6
Shubham Agrawal <shubhamagrawal154@gmail.com>
2020-08-05 23:00:18 +0530

    Create Best Time to Buy and Sell Stock.cpp

---
+++ Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = INT32_MAX;
+        int maxprofit = 0;
+        for(int i=0; i<prices.size(); i++){
+            minprice = min(minprice, prices[i]);
+            maxprofit = max(prices[i]-minprice, maxprofit);
+        }
+        return maxprofit;   
+    }
+};
